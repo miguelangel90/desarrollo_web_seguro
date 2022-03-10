e41a85c1cf9c3525b1d6ec79a73e4892771c6384
Daniel <d.munozr.2020@alumnos.urjc.es>
2022-03-10 20:19:19 +0100

    Prueba de estilos

--- src/main/java/com/example/GeniusApp/Models/Users/UAdmin.java
+++
@@ -1,4 +0,0 @@
-package com.example.GeniusApp.Models.Users;
-
-public class UAdmin extends User {
-}
--- src/main/java/com/example/GeniusApp/Models/Users/URegistered.java
+++
@@ -1,4 +0,0 @@
-package com.example.GeniusApp.Models.Users;
-
-public class URegistered extends User {
-}
