b38dadbf1c7637b57570c519b472a9e778f28a2f
Guillermo <guillermoprieto17@gmail.com>
2022-04-18 18:05:09 +0200

    Comments

--- src/main/java/com/example/GeniusApp/Models/Users/User.java
+++ src/main/java/com/example/GeniusApp/Models/Users/User.java
@@ -3,10 +3,18 @@ package com.example.GeniusApp.Models.Users;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 @Data
 @NoArgsConstructor
 public class User {
-    private long id = -1;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
     private String username;
     private String password;
 
