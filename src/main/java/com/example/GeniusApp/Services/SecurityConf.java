80064bbdf1d80f858f4e3e4e8975bc8579c8e2e9
Daniel <d.munozr.2020@alumnos.urjc.es>
2022-04-26 20:12:32 +0200

    protección XSS

---
+++ src/main/java/com/example/GeniusApp/Services/SecurityConf.java
@@ -0,0 +1,18 @@
+package com.example.GeniusApp.Services;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConf extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .headers()
+                .xssProtection()
+                .and()
+                .contentSecurityPolicy("script-src 'self'");
+    }
+}
